7c093ceedccef66ed3a905b66b0965c04a2c51ba
hahbihb <107093837+hahbihb@users.noreply.github.com>
2024-12-04 14:26:24 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$name="Habeeb";
+$age=12;
+$isAdult=true;
+$subjects=["Math","English","Civic"];
+echo "$name is $age years old <br>";
+echo "$name <br>";
+echo "$isAdult <br>";
+echo "$subjects[0],$subjects[1],$subjects[2] <br>"; 
+?>
